47abbcf111c01329c1d37c8b95e22ef0c134ffb7
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-27 21:21:29 +0300

    Initializing Bread on Board front-end web application with Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="text-warning">Hello, Gimmy !</h1>
+    <p class="text-danger">We will begin the project tomorrow !</p>
+</template>
