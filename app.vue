5fbc8bcb8cb21acc919560c11869b70998afa724
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-28 12:00:21 +0300

    Adding background image to application

--- app.vue
+++ app.vue
@@ -6,3 +6,11 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+  body {
+    background-image: url('~/assets/img/background-kitchen.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
