fac5a02c74add72d80572c727624965c26e5bab9
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-28 15:57:00 +0300

    Manage animations

--- app.vue
+++ app.vue
@@ -1,27 +1,12 @@
 <template>
   <div>
-    <NuxtLoadingIndicator/>
+    <NuxtLoadingIndicator color="#27AAFF"/>
     <NuxtRouteAnnouncer />
     <Header/>
     <NuxtPage />
   </div>
 </template>
 
-<style>
-  body {
-    background-image: url('~/assets/img/background-kitchen.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-  }
-
-  .page-enter-active,
-  .page-leave-active {
-    transition: all 0.4s;
-  }
-  .page-enter-from,
-  .page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-  }
-</style>
+<script setup>
+  import '~/assets/css/app.css';
+</script>
