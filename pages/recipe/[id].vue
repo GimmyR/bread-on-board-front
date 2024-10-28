95901107c777f85f1a736d2040ebc855ebaffdc8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-28 15:19:36 +0300

    Adding page to see a recipe

---
+++ pages/recipe/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container-fluid container-lg bg-light pt-5" style="min-height: 1200px;">
+    <h1>{{ route.params.id }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const route = useRoute();
+</script>
