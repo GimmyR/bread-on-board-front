d8188bf542b087c99c0ef441c056da253919ed20
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-28 15:16:34 +0300

    Centering position of background image

--- app.vue
+++ app.vue
@@ -12,6 +12,7 @@
     background-image: url('~/assets/img/background-kitchen.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    background-position: center;
   }
 
   .page-enter-active,
