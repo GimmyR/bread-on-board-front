806e44ec99a82a9ddebebfc672d1fb88698047e9
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-10-28 15:15:04 +0300

    Adding transition between pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   modules: ['usebootstrap', 'nuxt-bootstrap-icons']
 })
