94826cd79b328dea8f075ddbd35a091d0640c2f8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-11-02 12:12:49 +0300

    Search recipe

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -1,23 +1,36 @@
 <template>
   <Modal id="searchModal" size="lg">
     <ModalDialog fullscreen="md-down">
-      <ModalContent>
+      <ModalContent rounded-size="1">
         <ModalHeader>
-          <ModalTitle>Chercher une recette</ModalTitle>
+          <input type="text" class="form-control me-1" placeholder="Rechercher une recette" v-model="search" @keypress.enter="fetchRecipes">
           <CloseButton dismiss="modal" />
         </ModalHeader>
-        <ModalBody style="min-height: 600px;">
-          ...
+        <ModalBody class="px-4" style="height: 750px;">
+          <div class="d-flex flex-row align-items-center" v-for="recipe in recipes">
+            <Anchor class="fs-6 text-truncate my-auto py-2 text-decoration-none" :to="`/recipe/${recipe.id}`" dismiss="modal">
+              <span class="text-success me-3">{{ recipe.title }}</span> {{ recipe.ingredients }}
+            </Anchor>
+          </div>
         </ModalBody>
       </ModalContent>
     </ModalDialog>
   </Modal>
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+  const search = ref('');
+  const recipes = ref([]);
+  
+  const fetchRecipes = () => {
+    let form = new FormData();
+    form.append("search", search.value);
+    $fetch("http://localhost:9001/api/recipe/search/", {
+      method: 'POST',
+      body: form,
+      onResponse({ request, response, options }) {
+        recipes.value = response._data;
+      }
+    });
+  };
 </script>
-
-<style>
-
-</style>
