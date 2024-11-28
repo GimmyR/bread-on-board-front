7d2a406351a57a452a6f2acfbc10cd415d9a4695
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2024-11-28 14:12:24 +0300

    Improving aesthetic of login form

--- pages/login.vue
+++ pages/login.vue
@@ -2,21 +2,31 @@
   <div class="container-fluid container-lg pt-5">
     <div class="d-flex flex-row justify-content-center">
       <form @submit.prevent="logIn" class="d-flex flex-column align-items-center col-12 col-lg-8 mt-5 py-5 px-3 px-lg-0 bg-light">
+        <h1 class="text-success mb-5">Connexion</h1>
         <Transition name="alert">
           <div class="alert alert-danger col-12 col-lg-6 mb-4" role="alert" v-if="errorMessage != null">
             {{ errorMessage }}
           </div>
         </Transition>
         <div class="col-12 col-lg-6 mb-3">
-          <label for="username" class="form-label text-success">Nom d'utilisateur</label>
-          <input type="text" class="form-control" v-model="account.username"/>
+          <div class="input-group">
+            <span class="input-group-text">
+              <BIcon icon="bi:person" />
+            </span>
+            <input type="text" class="form-control" v-model="account.username" placeholder="Nom d'utilisateur"/>
+          </div>
         </div>
         <div class="col-12 col-lg-6 mb-4">
-          <label for="password" class="form-label text-success">Mots de passe</label>
-          <input type="password" class="form-control" v-model="account.password"/>
+          <div class="input-group">
+            <span class="input-group-text">
+              <BIcon icon="bi:lock" />
+            </span>
+            <input type="password" class="form-control" v-model="account.password" placeholder="Mots de passe"/>
+          </div>
         </div>
-        <div class="d-flex flex-row col-12 col-lg-6 justify-content-end">
-          <button type="submit" class="btn btn-success col-12 col-lg-5">Se connecter</button>
+        <div class="d-flex flex-column col-12 col-lg-6 mb-5">
+          <button type="submit" class="btn btn-success mb-2">Se connecter</button>
+          <button type="button" class="btn btn-dark" @click="signUp">S'inscrire</button>
         </div>
       </form>
     </div>
@@ -57,5 +67,9 @@
         } else errorMessage.value = response._data;
       }
     });
+  };
+
+  const signUp = () => {
+    router.push("#");
   };
 </script>
